4cada2f97b37608c69a0a39e64c206fd051d0cd2
tpsoe <tpsoe@cpp.edu>
2019-05-04 22:13:05 -0700

    done display

--- JDBC/Main.java
+++ JDBC/Main.java
@@ -1,23 +1,32 @@
 import java.io.*;
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 class Main{
-	private static final String url= "jdbc:mysql://localhost/Lab5";
+	
+	private static final String url= "jdbc:mysql://localhost/Lab5?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	private static final Scanner sc = new Scanner(System.in);
 	public static void main(String args[]){
+		String user="root";
+		String password = "iamnoob";
 		try{
-			Connection con = DriverManager.getConnection(url,args[0],args[1]);
+			
+			Connection con = DriverManager.getConnection(url,user,password);
 			Statement stmt = con.createStatement();
 			System.out.println("Success");
 			menu(stmt);
 			con.close();
 			stmt.close();
+			
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
-	public static void menu(Statement stmt){
+	public static void menu(Statement stmt) throws SQLException, ParseException{
 		boolean done=false;
 		while(!done){
 			System.out.print("1.Add Driver\n2.Delete Driver\n"+
@@ -29,6 +38,7 @@ class Main{
                     "12.add actual Trip stop info\n"+
                     "Enter x to close the application\n"+
 	              	"Choose Option : ");
+			
 		String option=sc.nextLine();
 			switch (option){
 				case "1":
@@ -67,6 +77,8 @@ class Main{
                 case "12":
                     add_actual_trip_stop_info(stmt);
                     break;
+                case "13":
+                	add_trip(stmt);
 				case "x":
                 case "X":
 					done =true;
@@ -77,8 +89,52 @@ class Main{
 			}
 		}
 	}
+	
+	public static void display_weekly_schedule_of_a_given_driver_and_date(Statement stmt) throws ParseException, SQLException {
+		System.out.print("Driver name: ");
+		String driver = sc.nextLine();
+		
+		System.out.print("Date: ");
+		String dateString = sc.nextLine();
+		
+		String varSql = "SELECT TripNumber, Date, ScheduledStartTime, ScheduleArrivalTime, BusID from TripOffering " + "where DriverName LIKE '" + driver + "' AND DATE_SUB('" + dateString + "',INTERVAL 7 DAY)" + " Order By ScheduledStartTime ";
+		
+		//query for selected driver and date
+		ResultSet rs = stmt.executeQuery(varSql);
+		ResultSetMetaData rsmd = rs.getMetaData();
+		int colCount = rsmd.getColumnCount();
+    	for(int i = 1; i <= colCount; i++){
+    	System.out.print(rsmd.getColumnName(i) + "\t");
+    	}
+    	System.out.println();
+    	//print out rows
+    	while(rs.next()){
+    	for(int i = 1; i <= colCount; i++)
+    	System.out.print(rs.getString(i) + "\t\t");
+    	System.out.println();
+    	}
+    	rs.close();
+		}
+	
+	public static void add_trip(Statement stmt) {
+		int result =0;
+		System.out.print("Enter the trip number : ");
+		String tripNumber = sc.nextLine();
+		System.out.print("Enter start location name : ");
+		String startLocation = sc.nextLine();
+		System.out.print("Enter destination location name : ");
+		String desLocation = sc.nextLine();
+		
+		String varSql = "insert into Trip(TripNumber,StartLocationName,DestinationName) values('" + tripNumber + "','" +
+		startLocation + "','" + desLocation +"')";
+		
+		result = updateDB(varSql, stmt);
+		
+		printMessage(result,"add trip");
+	}
+	
     public static void add_actual_trip_stop_info(Statement stmt){
-        int result = 0;
+    	int result = 0;
         System.out.print("Enter the Trip Number : ");
         String tripNumber = sc.nextLine();
         System.out.print("Enter the Date (YYYY-MM-DD) : ");
@@ -104,11 +160,10 @@ class Main{
         printMessage(result, "add into actual trip stop info");
 
     }
-    public static void display_weekly_schedule_of_a_given_driver_and_date(Statement stmt){
-    }
 
-    public static void display_stops_of_given_trip(Statement stmt) {
-        int result = 0;
+    public static void display_stops_of_given_trip(Statement stmt){
+    	
+    	int result = 0;
         System.out.print("Enter the Trip Number : ");
         String tripNumber = sc.nextLine();
         String varSql = ("select * from TripStopInfo where TripNumber="+tripNumber);
@@ -128,9 +183,34 @@ class Main{
         }catch(SQLException e){
 			e.printStackTrace();
 		}
-    }
-    public static void display_schedule_of_all_trips(Statement stmt){
 
+    }
+    public static void display_schedule_of_all_trips(Statement stmt) throws SQLException{
+    	System.out.print("Start Location Name: ");
+    	String startLoc = sc.nextLine();
+    	System.out.print("Destination Name: ");
+    	String destLoc = sc.nextLine();
+    	System.out.print("Date: ");
+    	String date = sc.nextLine();
+    	
+    	String varSql = "SELECT A.ScheduledStartTime, A.ScheduleArrivalTime, A.DriverName, A.BusID FROM TripOffering A, Trip B Where B.StartLocationName LIKE '" + startLoc + "' AND " + "B.DestinationName LIKE '" + destLoc + "' AND " + "A.Date = '" + date + "' AND " +
+    	"B.TripNumber = A.TripNumber ";
+    	
+    	ResultSet rs = stmt.executeQuery(varSql);
+    	ResultSetMetaData rsmd = rs.getMetaData();
+    	int colCount = rsmd.getColumnCount();
+    	for(int i = 1; i <= colCount; i++){
+    	System.out.print(rsmd.getColumnName(i) + "\t");
+    	}
+    	System.out.println();
+    	//print out rows
+    	while(rs.next()){
+    	for(int i = 1; i <= colCount; i++)
+    	System.out.print(rs.getString(i) + "\t\t");
+    	System.out.println();
+    	}
+    	rs.close();
+    	
     }
     public static void add_set_of_trip_offering(Statement stmt){
         boolean done = false;
@@ -138,11 +218,11 @@ class Main{
         do{
             System.out.print("Enter the Trip Number : ");
             String tripNumber = sc.nextLine();
-            System.out.print("Enter the Date(YYYY-MM-DD) : ");
+            System.out.print("Enter the Date : ");
             String date = sc.nextLine();
-            System.out.print("Enter the Scheduled Start Time (HH:MM:SS) : ");
+            System.out.print("Enter the Scheduled Start Time : ");
             String scheduledStartTime = sc.nextLine();
-            System.out.print("Enter the ScheduleArrival Time (HH:MM:SS) : ");
+            System.out.print("Enter the ScheduleArrival Time : ");
             String scheduleArrivalTime = sc.nextLine();
             System.out.print("Enter the Driver Name : ");
             String driverName = sc.nextLine();
@@ -163,9 +243,9 @@ class Main{
         int result=0;
         System.out.print("Enter the Trip Number : ");
         String tripNumber = sc.nextLine();
-        System.out.print("Enter the Date (YYYY-MM-DD) : ");
+        System.out.print("Enter the Date : ");
         String date = sc.nextLine();
-        System.out.print("Enter the Scheduled Start Time(HH:MM:SS) : ");
+        System.out.print("Enter the Scheduled Start Time : ");
         String scheduledStartTime = sc.nextLine();
         String varSql = "delete from TripOffering where TripNumber='" + tripNumber + "' and Date='" + date + "' and ScheduledStartTime='" + scheduledStartTime  + "'";
         result=updateDB(varSql,stmt);
@@ -176,7 +256,7 @@ class Main{
         int result=0;
         System.out.print("Enter the Trip Number : ");
         String tripNumber = sc.nextLine();
-        System.out.print("Enter the Date (YYYY-MM-DD) : ");
+        System.out.print("Enter the Date : ");
         String date = sc.nextLine();
         System.out.print("Enter the Scheduled Start Time : ");
         String scheduledStartTime = sc.nextLine();
@@ -192,9 +272,9 @@ class Main{
         int result=0;
         System.out.print("Enter the Trip Number : ");
         String tripNumber = sc.nextLine();
-        System.out.print("Enter the Date (YYYY-MM-DD) : ");
+        System.out.print("Enter the Date : ");
         String date = sc.nextLine();
-        System.out.print("Enter the Scheduled Start Time (HH:MM:SS) : ");
+        System.out.print("Enter the Scheduled Start Time : ");
         String scheduledStartTime = sc.nextLine();
         System.out.print("Enter the new BusID : ");
         String busID = sc.nextLine();
@@ -228,10 +308,10 @@ class Main{
         String busID=sc.nextLine();
         System.out.print("Enter the bus Model : ");
         String model=sc.nextLine();
-        System.out.print("Enter the year of bus (YYYY) : ");
+        System.out.print("Enter the year of bus : ");
         String year=sc.nextLine();
         while(!checkYear(year)){
-            System.out.print("Enter the Year again? (YYYY) : ");
+            System.out.print("Enter the Year again?(XXXX) : ");
             year=sc.nextLine();
         }
         String varSql = "insert into Bus(BusID,Model,Year) values('"+busID+"','"+model+"','"+year+"')";
@@ -255,9 +335,11 @@ class Main{
 		}
 		return result;
 	}
-    public static ResultSet queryDB(String varSql,Statement stmt) throws SQLException{
+	
+	public static ResultSet queryDB(String varSql,Statement stmt) throws SQLException{
         return  stmt.executeQuery(varSql);
     }
+
 	public static void success_message(){
 		System.out.println("\nSuccess!\n");
 	}
